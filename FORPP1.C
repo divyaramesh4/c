7913a625615fb8ed283cece9959b6df1955b767a
divyaramesh4 <129279073+divyaramesh4@users.noreply.github.com>
2023-11-19 15:24:09 +0530

    Add files via upload

---
+++ FORPP1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i,j ;
+  clrscr();
+  for(j=0;j<=3;j++)
+  {
+  for (i=6;i>=1;i--)
+  {
+   printf("* ");
+  }
+  printf("\n");
+
+  }
+ getch();
+ return 0;
+}
